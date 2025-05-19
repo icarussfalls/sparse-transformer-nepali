24e9abf42d590955948e715956285abd4964e5f0
icarus <pokhrel.pradip163@gmail.com>
2025-05-19 15:10:46 +0545

    added mixed precision training

--- train.py
+++ train.py
@@ -79,6 +79,11 @@ def get_or_build_tokenizer(config, ds, lang):
 def get_ds(config):
     # the data only has train split so
     ds_raw = load_dataset(f"{config['data_source']}", "default", split='train')
+
+    # lets use only 20% of the data
+    subset_size = int(0.2 * len(ds_raw))
+    ds_raw = ds_raw.select(range(subset_size))
+    print(f"Using {subset_size} samples out of {len(ds_raw)}")
     print(ds_raw[0])
 
     # build the tokenizers
