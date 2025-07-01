8779f45275a2a6c45a361abeb0955a347a50bfd0
icarus <pokhrel.pradip163@gmail.com>
2025-07-01 15:55:53 +0545

    checking some bugs

--- train.py
+++ train.py
@@ -16,6 +16,11 @@ import re
 
 # Huggingface datasets and tokenizers
 from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
 import sentencepiece as spm
 
 import torchmetrics
@@ -25,8 +30,8 @@ from torch.cuda.amp import autocast, GradScaler
 
 # simple greedy decode
 def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
-    sos_idx = tokenizer_tgt.piece_to_id("[SOS]")
-    eos_idx = tokenizer_tgt.piece_to_id("[EOS]") 
+    sos_idx = tokenizer_tgt.token_to_id("[SOS]")
+    eos_idx = tokenizer_tgt.token_to_id("[EOS]") 
 
     # precomputing the encoder output and reusing it for each step
     # encoder_output = model.encode(source, source_mask)
@@ -61,6 +66,19 @@ def get_all_sentences(ds, lang):
     for item in ds:
         yield item[lang]
 
+# def get_or_build_tokenizer(config, ds, lang):
+#     tokenizer_path = Path(config['tokenizer_file'].format(lang))
+#     if not Path.exists(tokenizer_path):
+#         # code from huggingface
+#         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+#         tokenizer.pre_tokenizer = Whitespace()
+#         trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+#         tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+#         tokenizer.save(str(tokenizer_path))
+#     else:
+#         tokenizer = Tokenizer.from_file(str(tokenizer_path))
+#     return tokenizer
+
 def get_or_build_tokenizer(config, ds, lang):
     model_prefix = f"spm_{lang}"
     model_file = f"{model_prefix}.model"
@@ -72,20 +90,19 @@ def get_or_build_tokenizer(config, ds, lang):
         
         # train sentencepiece model(unigram or use --model_type=bpe for bpe)
         spm.SentencePieceTrainer.Train(
-            f"--input={model_prefix}_train.txt --model_prefix={model_prefix} --vocab_size=6022 --model_type=unigram --character_coverage=1.0 --user_defined_symbols=[PAD],[UNK],[SOS],[EOS]"
+            f"--input={model_prefix}_train.txt --model_prefix={model_prefix} --vocab_size=32000 --model_type=unigram --character_coverage=1.0"
         )
-    sp = spm.SentencePieceProcessor()
-    sp.load(model_file)
-    return sp
+        sp = spm.SentencePieceProcessor()
+        sp.load(model_file)
+        return sp
 
 def get_ds(config):
     # the data only has train split so
     ds_all = load_dataset(f"{config['data_source']}", "default", split='train')
 
     # Shuffle and select a random 1% subset
-    subset_size = 0.1
     total_len = len(ds_all)
-    subset_size = int(0.1 * total_len)
+    subset_size = int(0.01 * total_len)
     indices = torch.randperm(total_len).tolist()[:subset_size]
     ds_raw = ds_all.select(indices)
     print(f"Using {subset_size} random samples out of {total_len}")
@@ -110,8 +127,8 @@ def get_ds(config):
     max_len_tgt = 0
     
     for item in ds_all:
-        src_ids = tokenizer_src.encode(item[config['lang_src']])
-        tgt_ids = tokenizer_tgt.encode(item[config['lang_tgt']])
+        src_ids = tokenizer_src.encode(item[config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item[config['lang_tgt']]).ids
         max_len_src = max(max_len_src, len(src_ids))
         max_len_tgt = max(max_len_tgt, len(tgt_ids))
 
@@ -167,7 +184,7 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
 
             source_text = batch['src_text'][0]
             target_text = batch['tgt_text'][0]
-            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy().tolist())
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
 
             source_texts.append(source_text)
             expected.append(target_text)
@@ -196,7 +213,6 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
         writer.add_scalar('validation wer', wer, global_step)
         writer.flush()
 
-        from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
         # compute the BLEU metric
         # Tokenize by splitting on whitespace
         # metric = torchmetrics.BLEUScore()
@@ -259,7 +275,7 @@ def train_model(config):
 
     train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
 
-    model = get_model(config, tokenizer_src.get_piece_size(), tokenizer_tgt.get_piece_size())
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size())
     
     # added gpu parallel support
     if torch.cuda.device_count() > 1 and device == "cuda":
@@ -293,7 +309,7 @@ def train_model(config):
     else:
         print(' No model to preload, starting from scratch')
 
-    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_tgt.piece_to_id('[PAD]'), label_smoothing=0.1).to(device)
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
 
     scaler = GradScaler() if device == "cuda" else None
 
@@ -315,17 +331,15 @@ def train_model(config):
             if scaler is not None:
                 with autocast():
                     proj_output = model(encoder_input, decoder_input, encoder_mask, decoder_mask)
-                    loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_piece_size()), label.view(-1))
+                    loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
                 scaler.scale(loss).backward()
                 scaler.step(optimizer)
                 scaler.update()
             else:
                 proj_output = model(encoder_input, decoder_input, encoder_mask, decoder_mask)
-                loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_piece_size()), label.view(-1))
+                loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
                 loss.backward()
                 optimizer.step()
-
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
 
             batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
             writer.add_scalar('train_loss', loss.item(), global_step)
@@ -345,12 +359,6 @@ def train_model(config):
                 'optimizer_state_dict': optimizer.state_dict(),
                 'global_step': global_step
             }, model_filename)
-
-    for batch in train_dataloader:
-        print(batch['encoder_input'])
-        print(batch['decoder_input'])
-        print(batch['label'])
-        break
 
 
 if __name__ == "__main__":
